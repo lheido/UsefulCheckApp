e4de6f0e98f6af1f749cd10b543b79defd25918f
lheido <lheido.web@gmail.com>
2015-02-27 10:04:22 +0100

    instance collections dans la fonction de rendu avec success callback

--- assets/js/views/checkin/list.js
+++ assets/js/views/checkin/list.js
@@ -8,11 +8,16 @@ define(
   ],
   // définition du scope.
   function($, _, Backbone, CheckInCollection) {
-
     var CheckInListView = Backbone.View.extend({
-      
+
       render: function() {
         console.log("CheckInView Render");
+        var checkInCollection = new CheckInCollection();
+        checkInCollection.fetch({
+          success: function(checkins) {
+            console.log(checkins.models);
+          }
+        });
       }
 
     });
